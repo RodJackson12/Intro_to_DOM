9c451066ecb5c3808078020e1e9ef598e17bd6bc
CITS <cits@CITSs-MBP.lan>
2024-10-28 18:01:57 -0400

    completed project

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-let links = document.querySelectorAll("a");
+let links = document.getElementsByTagName("a");
 console.log(links);
+let about = document.getElementById("about-content");
+console.log(about);
+let footer =  document.getElementsByClassName("footer");
+console.log(footer);
+let tag = document.getElementsByClassName("content")
+console.log(tag)
